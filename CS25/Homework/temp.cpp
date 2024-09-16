8a1b62660ea421a45b3bc1f3c00904c44eb890ac
AlexDude1015 <li.alexander13@gmail.com>
2024-09-16 15:53:15 -0700

    Pushing everything again

--- CS25/Homework/temp.cpp
+++ CS25/Homework/temp.cpp
@@ -2,18 +2,25 @@
 using namespace std;
 
 int main()
-{
+{   
+    //Asks user for a specific quantity of inputs
     int input_quantity;
-    cout << "How many inputs will you enter?";
+    cout << "How many inputs will you enter: ";
     cin >> input_quantity;
+
+    //Stores the sums of all inputs
     double total_temp = 0;
-    double placeholder_value = 1;
+
+    //Calculates the sum of every input
     cout << endl << "Please enter the temperatures: ";
-    for (int i = 1; i <= input_quantity; i++)
+    for (int i = 0; i < input_quantity; i++)
     {
+        double placeholder_value;
         cin >> placeholder_value;
         total_temp += placeholder_value;
-    }   
-    cout << total_temp / input_quantity;
+    }
+
+    //Prints the average
+    cout << "The average temperature is " << total_temp / input_quantity << endl;
     return 0;
 }
