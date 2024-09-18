bf0b472016ee1aae8ce0893c14779c3a24ed7de3
AlexDude1015 <li.alexander13@gmail.com>
2024-09-18 01:00:22 -0700

    secret number

--- CS25/Homework/secret_number.cpp
+++ CS25/Homework/secret_number.cpp
@@ -1,3 +1,4 @@
+//Users guess a random number and the program insults you each time you get it wrong.
 #include <iostream>
 #include <ctime>
 #include <string>
@@ -8,18 +9,23 @@ using namespace std;
 
 int main()
 {
+    //Creates random number
     srand(time(nullptr));
     int random_number = rand() & (1000) + 1;
+
+    //Generates 5 possible insults
+    string insults[5] = {"You suck bro!", "Guess again loser!!!", "What an idiot!", "Try again dummy", "Nope, are you having fun yet?"};
+
+    //Ask users to guess until they get it right.
     cout << "Please get the number between one and one thousand. ";
     int user_choice = 0;
-    string insults[5] = {"You suck bro!", "Guess again loser!!!", "What an idiot!", "Try again dummy", "Nope, are you having fun yet?"};
     while (user_choice != random_number)
     {
         cout << "What is your choice?" << endl;
         cin >> user_choice;
         if (user_choice == random_number) 
         {
-             cout << "Congratulations!" << endl;
+             cout << "Congratulations idiot!" << endl;
         }
         else
         {
