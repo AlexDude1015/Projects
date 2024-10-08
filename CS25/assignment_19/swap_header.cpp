c3e6e183d74f63900beef9d3350326849e105f42
AlexDude1015 <li.alexander13@gmail.com>
2024-10-07 22:07:51 -0700

    add comment

--- CS25/assignment_19/swap_header.cpp
+++ CS25/assignment_19/swap_header.cpp
@@ -1,5 +1,6 @@
 #include "swap_header.hpp"
 
+//Swaps two values; accepts two int pointers arguments
 void swap::swap_values(int* first_value, int* second_value)
 {
     int placeholder = *first_value;
