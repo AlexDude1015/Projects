4b04f02d449445195c72bac5d8ca20c3d754f436
AlexDude1015 <li.alexander13@gmail.com>
2024-10-07 22:09:41 -0700

    more comment

--- CS25/assignment_19/main.cpp
+++ CS25/assignment_19/main.cpp
@@ -3,18 +3,23 @@
 
 int main()
 {
+    //Starts with two variables
     int x = 2;
     int y = 7;
 
+    //Prints their values
+    std::cout << "\nThe value of x is " << x << "\n";
+    std::cout << "The value of y is " << y << "\n\n";
+
+    //Create pointers to hold them
     int* ptr_x = &x;
     int* ptr_y = &y;
 
-    std::cout << "\nThe value of x is " << x << "\n";
-    std::cout << "The value of y is " << y << "\n\n";
+    //Swaps them
     std::cout << "Swapping x & y\n\n";
-
     swap::swap_values(ptr_x, ptr_y);
 
+    //Prints the values again
     std::cout << "The value of x is " << x << "\n";
     std::cout << "The value of y is " << y << "\n\n";
     
