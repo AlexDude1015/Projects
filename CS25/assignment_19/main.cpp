8fa6b6a21fb77fe667ba0bd233b179db346c1ca2
AlexDude1015 <li.alexander13@gmail.com>
2024-10-07 22:11:56 -0700

    edit

--- CS25/assignment_19/main.cpp
+++ CS25/assignment_19/main.cpp
@@ -4,18 +4,18 @@
 int main()
 {
     //Starts with two variables
-    int x = 2;
+    int x = 12;
     int y = 7;
 
     //Prints their values
     std::cout << "\nThe value of x is " << x << "\n";
     std::cout << "The value of y is " << y << "\n\n";
 
-    //Create pointers to hold them
+    //Create two pointers to hold them
     int* ptr_x = &x;
     int* ptr_y = &y;
 
-    //Swaps them
+    //Swaps
     std::cout << "Swapping x & y\n\n";
     swap::swap_values(ptr_x, ptr_y);
 
--- CS25/assignment_19/swap_header.cpp
+++ CS25/assignment_19/swap_header.cpp
@@ -1,6 +1,6 @@
 #include "swap_header.hpp"
 
-//Swaps two values; accepts two int pointers arguments
+//Swaps the values in two pointers
 void swap::swap_values(int* first_value, int* second_value)
 {
     int placeholder = *first_value;
