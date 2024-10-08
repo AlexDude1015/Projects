a7de12514a16677e76c451a12aad46b972a38543
AlexDude1015 <li.alexander13@gmail.com>
2024-10-07 20:31:22 -0700

    debug

--- CS25/Class Assigments/class_analysis.cpp
+++ CS25/Class Assigments/class_analysis.cpp
@@ -5,10 +5,8 @@
 
 int main()
 {   
-    const int SIZE = 10;
-    int scores[SIZE] = {87, 69, 65, 74, 95, 83, 92, 97, 68, 100};
-    std::string names[SIZE] = {"Alex", "Bob", "Charlie", "David", "Evan", "Fred", "Greg", "Harold", "Isaac", "Jack"};
-    
-    std::cout << grade::get_highest_number(scores, SIZE);
+    //const int SIZE = 10;
+    //int scores[SIZE] = {87, 69, 65, 74, 95, 83, 92, 97, 68, 100};
+    //std::string names[SIZE] = {"Alex", "Bob", "Charlie", "David", "Evan", "Fred", "Greg", "Harold", "Isaac", "Jack"};
     return 0;
 }
